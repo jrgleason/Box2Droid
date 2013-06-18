1657b8dadb627cf043b110ff6d912c3e7665e11b
Your Name <jackiegleason@gmail.com>
2013-06-18 09:30:41 -0400

    Initial Commit

---
+++ Box2D/Box2D/Application.mk
@@ -0,0 +1,5 @@
+APP_PROJECT_PATH:=/Users/jackiegleason/Development/porting/ported/box2D/Box2D/Box2D
+APP_STL:=stlport_static
+APP_ABI := armeabi armeabi-v7a
+APP_PLATFORM := android-14
+APP_BUILD_SCRIPT=$(APP_PROJECT_PATH)/Android.mk
